88242bec225384396c870911220277ff814a34c4
forgottenentity <forgottenentity@hotmail.co.uk>
2019-02-09 02:19:42 +0000

    More Changes

--- src/arc_riak_connection.erl
+++ src/arc_riak_connection.erl
@@ -9,5 +9,90 @@
 -module(arc_riak_connection).
 -author("chris").
 
+-behaviour(gen_server).
+-behaviour(poolboy_worker).
+
 %% API
--export([]).
+
+-record(state, {riak_process}).
+
+-export([get/4,
+         get_meta/4,
+         put/3,
+         put/4
+        ]).
+
+-export([start_link/1]).
+
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2,
+  code_change/3]).
+
+%% **************************************************************************** %%
+%% API Functions %%
+%% **************************************************************************** %%
+
+get(Connection, Bucket, Key, Timeout) ->
+  gen_server:call(Connection, {get, {Bucket, Key, Timeout}}).
+
+get_meta(Connection, Bucket, Key, Timeout) ->
+  gen_server:call(Connection, {get_meta, {Bucket, Key, Timeout}}).
+
+put(Connection, Timeout, RiakObj) ->
+  gen_server:call(Connection, {put, {Timeout, RiakObj}}).
+
+put(Connection, Timeout, RiakObj, return_body) ->
+  gen_server:call(Connection, {put, {Timeout, RiakObj, return_body}});
+put(Connection, Timeout, RiakObj, return_head) ->
+  gen_server:call(Connection, {put, {Timeout, RiakObj, return_head}}).
+
+
+
+%% **************************************************************************** %%
+%% poolboy_worker functions %%
+%% **************************************************************************** %%
+
+
+start_link(Args) ->
+  gen_server:start_link(?MODULE, Args, []).
+
+
+%% **************************************************************************** %%
+%% gen_server functions %%
+%% **************************************************************************** %%
+
+init(Args) ->
+  Riak_PB_IP = proplists:get_value(riak_pb_ip, Args),
+  Riak_PB_Port = proplists:get_value(riak_pb_port, Args),
+  Riak_PB_Options= proplists:get_value(riak_pb_options, Args),
+
+  {ok, Pid} = riakc_pb_socket:start_link(Riak_PB_IP, Riak_PB_Port, Riak_PB_Options),
+  {ok, #state{riak_process = Pid}}.
+
+handle_call({get, {Bucket, Key, Timeout}}, _From, State = #state{riak_process=RiakProcess}) ->
+  Result = riakc_pb_socket:get(RiakProcess, Bucket, Key, Timeout),
+  {reply, Result, State};
+handle_call({get_meta, {Bucket, Key, Timeout}}, _From, State = #state{riak_process=RiakProcess}) ->
+  Result = riakc_pb_socket:get(RiakProcess, Bucket, Key, [head], Timeout),
+  {reply, Result, State};
+handle_call({put, {Timeout, RiakObj}}, _From, State = #state{riak_process=RiakProcess}) ->
+  Result = riakc_pb_socket:put(RiakProcess, RiakObj, Timeout),
+  {reply, Result, State};
+handle_call({put, {Timeout, RiakObj, return_body}}, _From, State = #state{riak_process=RiakProcess}) ->
+  Result = riakc_pb_socket:put(RiakProcess, RiakObj, [return_body], Timeout),
+  {reply, Result, State};
+handle_call({put, {Timeout, RiakObj, return_head}}, _From, State = #state{riak_process=RiakProcess}) ->
+  Result = riakc_pb_socket:put(RiakProcess, RiakObj, [return_head], Timeout),
+  {reply, Result, State}.
+
+handle_cast(_Msg, State) ->
+  {noreply, State}.
+
+handle_info(_Info, State) ->
+  {noreply, State}.
+
+code_change(_OldVsn, State, _Extra) ->
+  {ok, State}.
+
+terminate(_Reason, _State) ->
+  ok.
+
--- src/arc_riak_connection_pool.erl
+++ src/arc_riak_connection_pool.erl
@@ -9,5 +9,60 @@
 -module(arc_riak_connection_pool).
 -author("chris").
 
+-behaviour(supervisor).
+
+%% Supervisor exports
+
+-export([
+         init/1
+        ]).
+
+-define(pool_name, riak_pool).
+-define(pool_size_args, [{size, 400}, {max_overflow, 400}]).
+-define(pool_worker_args, []).
+
+
+
 %% API
--export([]).
+-export([get_connection/1,
+         return_connection/2
+        ]).
+
+
+
+init(_Args) ->
+  {ok, Config} = application:get_env(arc_riak),
+
+  io:format("Config: ~n~p", [Config]),
+
+  PoolSpecs = lists:map(fun(PoolConfigPropList) ->
+    Name = proplists:get_value(riak_pool_name, PoolConfigPropList),
+    Size = proplists:get_value(riak_pool_base_size, PoolConfigPropList),
+    MaxOverFlow = proplists:get_value(riak_pool_max_growth, PoolConfigPropList),
+    PB_IP = proplists:get_value(riak_pb_ip, PoolConfigPropList),
+    PB_Port = proplists:get_value(riak_pb_port, PoolConfigPropList),
+    PB_Options = proplists:get_value(riak_pb_options, PoolConfigPropList),
+    SizeArgs = [{size, Size}, {max_overflow, MaxOverFlow}],
+    WorkerArgs = [{riak_pb_ip, PB_IP}, {riak_pb_port, PB_Port}, {riak_pb_options, PB_Options}],
+
+    PoolArgs = [{name, {local, Name}},
+      {worker_module, arc_riak_connection}] ++ SizeArgs,
+    poolboy:child_spec(Name, PoolArgs, WorkerArgs)
+                        end, Config),
+
+  io:format("Poolboy Spec: ~p~n", PoolSpecs),
+
+  {ok, {{one_for_one, 10, 10}, PoolSpecs}}.
+
+
+%% API Functions %%
+
+-spec get_connection(RiakPoolName :: atom()) -> pid().
+get_connection(RiakPoolName) ->
+  poolboy:checkout(RiakPoolName).
+
+-spec return_connection(RiakPoolName :: atom(), Connection :: pid()) -> ok.
+return_connection(RiakPoolName, Connection) ->
+  poolboy:checkin(RiakPoolName, Connection).
+
+
